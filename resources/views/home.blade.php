3a94a3e1778435e09cfc74133c2589590dff06d1
DamianoD88 <dispe88@live.it>
2021-09-06 22:13:46 +0200

    add prove

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!-- estendo il layouts -->
+@exetends('layouts.app')
+
+@section('title')
+@section('content')
+    <div class="foto">
+        <h1>Home</h1>
+    </div>
+    <section>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta nisi ullam molestiae suscipit tenetur veniam nulla inventore temporibus ea adipisci mollitia, deserunt earum repudiandae aut sint praesentium provident alias dolor.
+        </p>
+    </section>
+@endsection
